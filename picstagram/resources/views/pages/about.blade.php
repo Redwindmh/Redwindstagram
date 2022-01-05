6a1ea55b5f32277a92302f66e85fed5b5382af84
Malcolm <redwindmh@gmail.com>
2022-01-05 18:59:25 +0900

    More responsive changes

--- picstagram/resources/views/pages/about.blade.php
+++ picstagram/resources/views/pages/about.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row text-center">
-        <div class="col-md-8">
+<div class="container text-center">
+    <div class="row">
+        <div class="mt-3">
             <h2>About</h2>
-            <p>Thanks for checking out Redwindstagram!</p>
+            <div class="text-start px-3">
+                <p>Thanks for checking out Redwindstagram! Please let me know if you have any problems with the site or have any suggestions.<br><br>-RW</p>
+            </div>
         </div>
     </div>
 </div>
