018fe12ae066f7c2af92c0ba74c5e12fd49046a3
Malcolm <redwindmh@gmail.com>
2022-01-20 12:18:53 +0900

    Test change

--- picstagram/resources/views/emails/welcome-email.blade.php
+++ picstagram/resources/views/emails/welcome-email.blade.php
@@ -4,7 +4,7 @@ Thanks for joining. Click the button below. Or don't. Your call, really.
 
 
 @component('mail::button', ['url' => 'https://www.youtube.com/watch?v=kZwhNFOn4ik', 'color' => 'error'])
-Click me!
+Click me!!
 @endcomponent
 Sincerely,<br>
 The <span style="color:red;">{{ config('app.name') }}</span> Team (lol it's just me)
