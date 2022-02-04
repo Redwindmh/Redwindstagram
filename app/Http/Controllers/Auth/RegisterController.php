4df063145667233aee330e76cd829cc6d502414a
Malcolm Hendricks <malcolm@r-nc.co.jp>
2022-02-04 19:05:27 +0900

    post-registration route fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/profile/{$user->username}';
 
     /**
      * Create a new controller instance.
