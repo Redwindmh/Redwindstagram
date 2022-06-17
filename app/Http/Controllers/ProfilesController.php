936a0a6f9da6eaa5053a7be9d6df7e716c58145a
Malcolm Hendricks <redwindmh@gmail.com>
2022-06-17 22:22:26 +0900

    First shot at getting S3 storage to work

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -12,28 +12,30 @@ class ProfilesController extends Controller
     {
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
 
-        $postCount = Cache::remember('count.posts.' . $user->id, now()->addSeconds(30), function() use ($user) {
+        $postCount = Cache::remember('count.posts.' . $user->id, now()->addSeconds(30), function () use ($user) {
             return $user->posts->count();
         });
 
-        $followersCount = Cache::remember('count.followers.' . $user->id, now()->addSeconds(10), function() use ($user) {
+        $followersCount = Cache::remember('count.followers.' . $user->id, now()->addSeconds(10), function () use ($user) {
             return $user->profile->followers->count();
         });
 
-        $followingCount = Cache::remember('count.following.' . $user->id, now()->addSeconds(10), function() use ($user) {
+        $followingCount = Cache::remember('count.following.' . $user->id, now()->addSeconds(10), function () use ($user) {
             return $user->following->count();
         });
 
         return view('profiles.index', compact('user', 'follows', 'postCount', 'followersCount', 'followingCount'));
     }
 
-    public function edit(\App\Models\User $user) {
+    public function edit(\App\Models\User $user)
+    {
         $this->authorize('update', $user->profile);
 
         return view('profiles.edit', compact('user'));
     }
 
-    public function update(\App\Models\User $user) {
+    public function update(\App\Models\User $user)
+    {
         $this->authorize('update', $user->profile);
 
 
@@ -46,7 +48,7 @@ class ProfilesController extends Controller
 
 
         if (request('image')) {
-            $imagePath = request('image')->store('profile', 'public');
+            $imagePath = request('image')->store('profile', 's3');
 
             $image = Image::make(public_path("/storage/{$imagePath}"));
             $image->save();
