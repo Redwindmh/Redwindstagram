c9ed275381f8c1df11d12d018c4155dcda0d6c80
Malcolm Hendricks <redwindmh@gmail.com>
2023-06-19 19:09:24 +0900

    dffer

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,8 +38,8 @@ class PostController extends Controller
         $imagePath = request('image')->store('posts', 's3');
 
         // $image = Image::make(public_path($imagePath))->fit(1200, 1200);
-        $image = Image::make(storage_path($imagePath));
-        // $image = Image::make(Storage::url($imagePath))->fit(1200, 1200);
+        // $image = Image::make(storage_path($imagePath));
+        $image = Image::make(Storage::url($imagePath))->fit(1200, 1200);
 
         $image->save();
 
