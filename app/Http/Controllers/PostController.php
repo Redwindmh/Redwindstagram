d067d5886b528e96caff9632f38a6138cb97717c
Malcolm Hendricks <redwindmh@gmail.com>
2023-06-19 16:54:58 +0900

    And again

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,35 +28,59 @@ class PostController extends Controller
         return view('posts.create');
     }
 
-    public function store()
+    // public function store()
+    // {
+    //     $data = request()->validate([
+    //         'caption' => 'required',
+    //         'image' => ['required', 'image'],
+    //     ]);
+
+    //     $imagePath = request('image')->store('posts', 's3');
+
+    //     // $image = Image::make(public_path("/storage/{$imagePath}"))->fit(1200, 1200);
+    //     // $image = Image::make(public_path($imagePath))->fit(1200, 1200);
+    //     // $image = Image::make(Storage::url($imagePath))->fit(1200, 1200);
+
+    //     // $image->save();
+
+    //     $image = Image::make(public_path($imagePath));
+    //     $image->fit(1200, 1200);
+    //     $image->save(public_path($imagePath));
+
+
+    //     auth()->user()->posts()->create([
+    //         'caption' => $data['caption'],
+    //         'image' => $imagePath,
+    //     ]);
+
+    //     dd($imagePath);
+
+    //     return redirect('/profile/' . auth()->user()->username);
+    // }
+
+    public function store(Request $request)
     {
-        $data = request()->validate([
-            'caption' => 'required',
-            'image' => ['required', 'image'],
-        ]);
+        if ($request->hasFile('profile_image')) {
+            //get filename with extension
+            $filenamewithextension = $request->file('profile_image')->getClientOriginalName();
+            //get filename without extension
+            $filename = pathinfo($filenamewithextension, PATHINFO_FILENAME);
+            //get file extension
+            $extension = $request->file('profile_image')->getClientOriginalExtension();
+            //filename to store
+            $filenametostore = $filename . '_' . uniqid() . '.' . $extension;
+            Storage::put('public/profile_images/' . $filenametostore, fopen($request->file('profile_image'), 'r+'));
+            Storage::put('public/profile_images/crop/' . $filenametostore, fopen($request->file('profile_image'), 'r+'));
+            //Crop image here
+            $cropimage = public_path('storage/profile_images/crop/' . $filenametostore);
+            $img = Image::make($cropimage)->crop($request->input('w'), $request->input('h'), $request->input('x1'), $request->input('y1'))->save($cropimage);
 
-        $imagePath = request('image')->store('posts', 's3');
-
-        // $image = Image::make(public_path("/storage/{$imagePath}"))->fit(1200, 1200);
-        // $image = Image::make(public_path($imagePath))->fit(1200, 1200);
-        // $image = Image::make(Storage::url($imagePath))->fit(1200, 1200);
-
-        // $image->save();
-
-        $image = Image::make(public_path($imagePath));
-        $image->fit(1200, 1200);
-        $image->save(public_path($imagePath));
-
-
-        auth()->user()->posts()->create([
-            'caption' => $data['caption'],
-            'image' => $imagePath,
-        ]);
-
-        dd($imagePath);
-
-        return redirect('/profile/' . auth()->user()->username);
+            // you can save the below image path in database
+            $path = asset('storage/profile_images/crop/' . $filenametostore);
+            return redirect('image')->with(['success' => "Image cropped successfully.", 'path' => $path]);
+        }
     }
+
 
 
     // public function store(Request $request)
