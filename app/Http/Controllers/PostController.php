3dc5114052970a610840136c059bed3a3cf4aee3
Malcolm Hendricks <redwindmh@gmail.com>
2023-06-19 17:33:53 +0900

    Ugh

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,10 +38,10 @@ class PostController extends Controller
         $imagePath = request('image')->store('posts', 's3');
 
         // $image = Image::make(public_path("/storage/{$imagePath}"))->fit(1200, 1200);
-        $image = Image::make(public_path($imagePath))->fit(1200, 1200);
+        $image = Image::make($imagePath)->fit(1200, 1200);
         // $image = Image::make(Storage::url($imagePath))->fit(1200, 1200);
 
-        // $image->save();
+        $image->save();
 
         auth()->user()->posts()->create([
             'caption' => $data['caption'],
